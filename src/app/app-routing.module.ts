1aa83cd753c387dd9521b6091d596d4c923f1cfa
Brian Mikinski <brian.mikinski@gmail.com>
2020-12-22 07:30:11 -0600

    adding routing to component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoanFormComponent } from './loan-form/loan-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'loanform', component: LoanFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
